494bac0cffadfd7de4cf1429f22cffca59420af2
jcbalbdev <jcbalbdev@gmail.com>
2025-04-21 23:45:54 -0500

    Actualización automática: 2025-04-21 23:45:54

--- lv2-usando-const.py
+++ lv2-usando-const.py
@@ -1,9 +1,9 @@
-""" Declara una constante para el valor de PI y úsala para calcular el área de un círculo con radio 4. """
+""" Declara una constante para el valor de PI y úsala para calcular el área de un círculo con radio 4. muestra solo dos decimales """
 
 PI: float = 3.141592
 RADIO_CIRCULO: int = 4
 
 area_circulo: float = PI*RADIO_CIRCULO**2
 
-print(f"el radio del circulo es {RADIO_CIRCULO} y su area es {area_circulo}")
+print(f"el radio del circulo es {RADIO_CIRCULO} y su area es {area_circulo:.2f}")
 
