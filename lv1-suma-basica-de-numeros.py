47c9e4476369c1c1b0fd4c75eb635dbc34a772b3
jcbalbdev <jcbalbdev@gmail.com>
2025-04-21 23:02:24 -0500

    Actualización automática: 2025-04-21 23:02:24

--- lv1-suma-basica-de-numeros.py
+++ lv1-suma-basica-de-numeros.py
@@ -1,1 +1,7 @@
+""" Declara dos variables numéricas y muestra su suma. """
+numero_uno: int = 2
+numero_dos: int = 10
 
+suma: int = numero_uno + numero_dos
+
+print(f"la suma de {numero_uno} y {numero_dos} es {suma}")
