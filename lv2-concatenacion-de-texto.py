e84059c0d884ef0800f2ef841da077bf1d34add3
jcbalbdev <jcbalbdev@gmail.com>
2025-04-21 23:08:33 -0500

    Actualización automática: 2025-04-21 23:08:33

--- lv2-concatenacion-de-texto.py
+++ lv2-concatenacion-de-texto.py
@@ -1,1 +1,8 @@
+""" Declara tres variables: nombre, apellido y edad. Usa template literals para formar un mensaje como:
+"Hola, soy Juan Pérez y tengo 25 años." """
 
+nombre: str = "Barry"
+apellido: str = "Allen"
+edad: int = 32
+
+print(f"Hola, soy {nombre} {apellido} y tengo {edad} años.")
