d8d26de99cfb1863f94b36dc0205f76cf466f23d
jcbalbdev <jcbalbdev@gmail.com>
2025-04-21 22:51:17 -0500

    Actualización automática: 2025-04-21 22:51:16

--- lv1-declara-tus-primeras-variables.py
+++ lv1-declara-tus-primeras-variables.py
@@ -1,3 +1,5 @@
-""" Declara una variable nombre de tipo string, edad de tipo number y esEstudiante de tipo boolean."""
+""" Declara una variable nombre de tipo string, edad de tipo number y esEstudiante de tipo boolean. muestralo en consola"""
 
-
+nombre: str = "luke"
+edad: int = 23
+esEstudiante: bool = True
