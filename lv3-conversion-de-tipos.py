4fabe0fdf7b1f017e51e419e4e31a0a50837c31d
jcbalbdev <jcbalbdev@gmail.com>
2025-04-21 23:12:38 -0500

    Actualización automática: 2025-04-21 23:12:38

--- lv3-conversion-de-tipos.py
+++ lv3-conversion-de-tipos.py
@@ -1,1 +1,7 @@
+""" Declara una variable tipo string con valor "123", conviértela a número y realiza una operación matemática. """
 
+numero_str: str = "123"
+
+mi_numero: int = int(numero_str)
+
+print(f"mi string es {numero_str} y lo acabo de convertir a entero y es igual a {mi_numero} ")
